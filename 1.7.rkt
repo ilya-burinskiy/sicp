5398786ff3cc59f5edf23995b2315965e84cd313
Ilya Burinsky <burinskiy992@gmail.com>
2022-12-14 23:48:53 +0300

    (wip): 1.7.rkt

---
+++ 1.7.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(define (sqrt x)
+  (define (sqrt-iter guess x)
+    (define (good-enough? guess x)
+      (< (abs (- (square guess) x)) 0.001))
+    (define (improve guess x)
+      (average guess (/ x guess)))
+    (if (good-enough? guess x)
+        guess
+        (sqrt-iter (improve guess x)
+                   x)))
+  (sqrt-iter 1.0 x))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (square x)
+  (* x x))

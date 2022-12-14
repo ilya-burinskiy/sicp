efa5224b3ea9f09c8420d0f1182dd56eb9f318c9
Ilya Burinsky <burinskiy992@gmail.com>
2022-12-14 23:53:58 +0300

    init guess

--- 1.7.rkt
+++ 1.7.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (define (sqrt x)
+  (define init-guess 1.0)
   (define (sqrt-iter current-guess x)
     (define (good-enough? current-guess x)
       (< (abs (- (square current-guess) x)) 0.001))
@@ -10,7 +11,7 @@
         current-guess
         (sqrt-iter (improve current-guess x)
                    x)))
-  (sqrt-iter 1.0 x))
+  (sqrt-iter init-guess x))
 
 (define (average x y)
   (/ (+ x y) 2))

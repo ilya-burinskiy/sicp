6a931869196cf7c3817396810ab0edb50af1d0b5
Ilya Burinsky <burinskiy992@gmail.com>
2022-12-15 00:02:43 +0300

    removed x param from nested defns

--- 1.7.rkt
+++ 1.7.rkt
@@ -2,17 +2,16 @@
 
 (define (sqrt x)
   (define init-guess 1.0)
-  (define (sqrt-iter prev-guess current-guess x)
-    (define (good-enough? prev-guess current-guess x)
+  (define (sqrt-iter prev-guess current-guess)
+    (define (good-enough? prev-guess current-guess)
       (< (abs (- (square current-guess) x)) 0.001))
-    (define (improve guess x)
+    (define (improve guess)
       (average guess (/ x guess)))
-    (if (good-enough? prev-guess current-guess x)
+    (if (good-enough? prev-guess current-guess)
         current-guess
         (sqrt-iter current-guess
-                   (improve current-guess x)
-                   x)))
-  (sqrt-iter 0.0 init-guess x))
+                   (improve current-guess))))
+  (sqrt-iter 0.0 init-guess))
 
 (define (average x y)
   (/ (+ x y) 2))

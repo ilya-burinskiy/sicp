3869ffae293ba4118e035be8a1d81d625955fe6d
Ilya Burinsky <burinskiy992@gmail.com>
2022-12-14 23:58:03 +0300

    prev guess

--- 1.7.rkt
+++ 1.7.rkt
@@ -2,16 +2,17 @@
 
 (define (sqrt x)
   (define init-guess 1.0)
-  (define (sqrt-iter current-guess x)
-    (define (good-enough? current-guess x)
+  (define (sqrt-iter prev-guess current-guess x)
+    (define (good-enough? prev-guess current-guess x)
       (< (abs (- (square current-guess) x)) 0.001))
     (define (improve guess x)
       (average guess (/ x guess)))
-    (if (good-enough? current-guess x)
+    (if (good-enough? prev-guess current-guess x)
         current-guess
-        (sqrt-iter (improve current-guess x)
+        (sqrt-iter current-guess
+                   (improve current-guess x)
                    x)))
-  (sqrt-iter init-guess x))
+  (sqrt-iter 0.0 init-guess x))
 
 (define (average x y)
   (/ (+ x y) 2))

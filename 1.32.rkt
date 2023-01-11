8a8dff8a7a57900199a9e450fc5ee2f4bd60f7e5
Ilya Burinsky <burinskiy992@gmail.com>
2023-01-11 21:50:05 +0300

    1.32

---
+++ 1.32.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+                (accumulate combiner
+                            null-value
+                            term
+                            (next a)
+                            next
+                            b))))
+
+(define (accumulate-iterative combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner (term a) result))))
+  (iter a null-value))

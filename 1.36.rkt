8671dc44f4a5446f9e85533376b6751a51f59265
Ilya Burinsky <burinskiy992@gmail.com>
2023-01-21 14:27:27 +0300

    1.36

---
+++ 1.36.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(define tolerance 1e-5)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (display next)
+      (newline)
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))

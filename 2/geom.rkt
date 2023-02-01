d0e3c3ceeb1bf8486cae15936b997b37bcb5f612
Ilya Burinsky <burinskiy992@gmail.com>
2023-02-01 21:31:08 +0300

    2.3

--- 2/geom.rkt
+++ 2/geom.rkt
@@ -21,3 +21,23 @@
           (yc (/ (+ y1 y2) 2)))
       (make-point xc yc))))
 
+(define (make-rectangle p1 p2)
+  (cons p1 p2))
+
+(define (rectangle-p1 rect) (car rect))
+
+(define (rectangle-p2 rect) (cdr rect))
+
+(define (rectangle-area rect)
+  (let ((p1 (rectangle-p1 rect))
+        (p2 (rectangle-p2 rect)))
+    (let ((a (- (x-point p2) (x-point p1)))
+          (b (- (y-point p2) (y-point p1))))
+      (* a b))))
+
+(define (rectangle-perimeter rect)
+  (let ((p1 (rectangle-p1 rect))
+        (p2 (rectangle-p2 rect)))
+    (let ((a (- (x-point p2) (x-point p1)))
+          (b (- (y-point p2) (y-point p1))))
+      (* 2 (+ a b)))))

3541e82cf811485dd874e0169ce7562c8f6877f1
Ilya Burinsky <burinskiy992@gmail.com>
2023-02-07 20:13:48 +0300

    2.12

--- 2/interval.rkt
+++ 2/interval.rkt
@@ -35,3 +35,16 @@
 (define (lower-bound i) (car i))
 
 (define (upper-bound i) (cdr i))
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (upper-bound i) (lower-bound)) 2))
+
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+(define (make-center-percent c p)
+  (let ((err (* c (/ p 100.0))))
+    (make-interval (- c err) (+ c err))))

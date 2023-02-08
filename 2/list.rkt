500408205670604d7daa798eb68bc7dfcf753686
Ilya Burinsky <burinskiy992@gmail.com>
2023-02-08 21:02:56 +0300

    2.17

--- 2/list.rkt
+++ 2/list.rkt
@@ -1,5 +1,14 @@
 #lang racket
 
+(provide last-pair reverse-list)
+
 (define (last-pair lst)
   (cond ((= (cdr lst) null) (car lst))
         (else (last-pair (cdr lst)))))
+
+(define (reverse-list lst)
+  (define (reverse-list-helper lst res)
+    (if (null? lst)
+        res
+        (reverse-list-helper (cdr lst) (cons (car lst) res))))
+  (reverse-list-helper lst null))

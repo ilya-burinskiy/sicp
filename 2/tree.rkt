1950c1f22ba2bbd010891bbe659044562f0f583f
Ilya Burinsky <burinskiy992@gmail.com>
2023-02-15 21:00:21 +0300

    2.31

--- 2/tree.rkt
+++ 2/tree.rkt
@@ -1,16 +1,12 @@
 #lang racket
 
-(define (square x) (* x x))
-
-(define (square-tree-1 tree)
-  (cond ((null? tree) null)
-        ((not (pair? tree)) (square tree))
-        (else (cons (square-tree-1 (car tree))
-                    (square-tree-1 (cdr tree))))))
-
-(define (square-tree-2 tree)
+(define (tree-map f tree)
   (map (lambda (sub-tree)
          (if (pair? sub-tree)
-             (square-tree-2 sub-tree)
-             (square sub-tree)))
+             (tree-map f sub-tree)
+             (f sub-tree)))
        tree))
+
+(define (square-tree tree)
+  (tree-map (lambda (x) (* x x)) tree))
+

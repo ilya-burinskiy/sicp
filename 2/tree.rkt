5f131a417460551dd867c7a786b5f789acefd1b7
Ilya Burinsky <burinskiy992@gmail.com>
2023-02-18 13:05:23 +0300

    2.35

--- 2/tree.rkt
+++ 2/tree.rkt
@@ -1,5 +1,8 @@
 #lang racket
 
+(require "sequence.rkt")
+(provide tree-map square-tree count-leaves)
+
 (define (tree-map f tree)
   (map (lambda (sub-tree)
          (if (pair? sub-tree)
@@ -10,3 +13,10 @@
 (define (square-tree tree)
   (tree-map (lambda (x) (* x x)) tree))
 
+(define (count-leaves tree)
+  (accumulate + 0 (map
+                    (lambda (sub-tree)
+                      (if (pair? sub-tree)
+                          (count-leaves sub-tree)
+                          1))
+                    tree)))

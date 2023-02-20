f0c5c0cdc64e79bf82ac9e3cb567ee3e4a407101
Ilya Burinsky <burinskiy992@gmail.com>
2023-02-20 22:38:57 +0300

    added fold-right and fold-left
    realized accumulate with right fold

--- 2/sequence.rkt
+++ 2/sequence.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(provide filter accumulate accumulate-n append length)
+(provide filter accumulate fold-right fold-left accumulate-n append length)
 
 (define (filter predicate sequence)
   (cond ((null? sequence) null)
@@ -10,10 +10,21 @@
         (else (filter predicate (cdr sequence)))))
 
 (define (accumulate op initial sequence)
+  (fold-right op initial sequence))
+
+(define (fold-right op initial sequence)
   (if (null? sequence)
       initial
       (op (car sequence)
           (accumulate op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
 
 (define (accumulate-n op init seqs)
   (if (null? (car seqs))

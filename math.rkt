ae6c6175cc58eb5187b8741bd9c678cee03ff6f6
Ilya Burinsky <burinskiy992@gmail.com>
2023-02-23 21:35:04 +0300

    added square, gcd, find-divisor, divides? and prime?

--- math.rkt
+++ math.rkt
@@ -1,8 +1,26 @@
 #lang racket
 
-(provide gcd)
+(provide gcd square find-divisor divides? prime?)
 
 (define (gcd a b)
   (if (= b 0)
       a
       (gcd b (remainder a b))))
+
+(define (square x) (* x x))
+
+(define (smallest-divisor n) (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (define (next divisor)
+    (cond ((= divisor 2) 3)
+          (else (+ divisor 2))))
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))

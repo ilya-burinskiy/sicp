71f3541bd7b94d997a9df070bbb5f3f356c0912f
Ilya Burinsky <burinskiy992@gmail.com>
2023-04-18 14:28:28 +0300

    2.61

--- set.rkt
+++ set.rkt
@@ -2,20 +2,28 @@
 
 (define (element-of-set? x set)
   (cond [(null? set) false]
-        [(equal? x (car set)) true]
+        [(= x (car set)) true]
+        [(< x (car set)) false]
         [else (element-of-set? x (cdr set))]))
 
 (define (adjoin-set x set)
-  (if (element-of-set? x set)
-      set
-      (cons x set)))
+  (cond [(null? set) (cons x set)]
+        [(= x (car set)) set]
+        [(< x (car set)) (cons x set)]
+        [else (cons (car set)
+                    (adjoin-set x (cdr set)))]))
 
 (define (intersection-set set1 set2)
-  (cond [(or (null? set1) (null? set2)) '()]
-        [(element-of-set? (car set1) set2)
-         (cons (car set1)
-               (intersection-set (cdr set1) set2))]
-        [else (intersection-set (cdr set1) set2)]))
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1))
+            (x2 (car set2)))
+        (cond [(= x1 x2)
+               (cons x1
+                     (intersection-set (cdr set1)
+                                       (cdr set2)))]
+              [(< x1 x2) (intersection-set (cdr set1) set2)]
+              [(> x1 x2) (intersection-set set1 (cdr set2))])))))
 
 (define (union-set set1 set2)
   (cond [(null? set1) set2]

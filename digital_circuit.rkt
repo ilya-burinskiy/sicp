bda88950073e3b47e8b8848b72241e4495881598
Ilya Burinsky <burinskiy992@gmail.com>
2025-07-01 18:20:06 +0300

    3.28

--- digital_circuit.rkt
+++ digital_circuit.rkt
@@ -19,6 +19,7 @@
 
 (define inverter-delay 1)
 (define and-gate-delay 1)
+(define or-gate-delay 1)
 
 (define (inverter input output)
   (define (invert-input)
@@ -75,3 +76,25 @@
     (half-adder a s sum c2)
     (or-gate c1 c2 c-out)
     'ok))
+
+(define (or-gate a1 a2 output)
+  (define (or-action-proc)
+    (let ([new-val
+            (logical-or (get-signal a1)
+                        (get-signal a2))])
+      (after-delay
+        or-gate-delay
+        (lambda ()
+          (set-signal! output new-val)))))
+  (add-action! a1 or-action-proc)
+  (add-action! a2 or-action-proc)
+  'ok)
+
+(define (logical-or s1 s2)
+  (cond
+    [(= s1 s2 1) 1]
+    [(= s1 s2 0) 0]
+    [(or (and (= s1 1) (= s2 0))
+         (and (= s1 0) (= s2 1)))
+     1]
+    [else (error "Invalid signals" s1 s2)]))
